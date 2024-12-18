fba6f1ed0c2331ac14d8327de975539be9e501cf
ravenkim <41793830+ravenkim@users.noreply.github.com>
2024-12-18 18:00:24 +0900

    ㄴㄴ

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import {Outlet} from "react-router";
-import SSlayout from "@/widgets/layout/SSlayout.tsx";
+import SSlayout from "@/layout/components/SSlayout.tsx";
 
 const Layout = () => {
     return (
