399909703fa9e2bd208e697b84fae15b7f55f92a
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-04-04 14:25:09 +0900

    스토리북 추가 투두리스트 작성

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path";
@@ -7,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss()],
+
 
   // 포트지정
   server: {
