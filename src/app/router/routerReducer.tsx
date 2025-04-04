4220b58a7848e3259aa6cbc64b82f66d72e75127
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-04-04 10:59:31 +0900

    프리티어 적용

--- src/app/router/routerReducer.tsx
+++ src/app/router/routerReducer.tsx
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction, Slice } from '@reduxjs/toolkit'
 
 const prefix = 'router'
 
-const asyncRequests = [] as const;
+const asyncRequests = [] as const
 
 const localState = {
     location: {
@@ -14,7 +14,7 @@ const localState = {
 
 const localReducers = {
     locationChange: (state: typeof localState, action: PayloadAction<{
-        path:string,
+        path: string,
         state: unknown
     }>) => {
         return {
