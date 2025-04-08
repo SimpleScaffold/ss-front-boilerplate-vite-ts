db4681528ecd8935e703ae33e706b28d44a0723e
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-04-08 15:45:21 +0900

    폰트 preload

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path";
 import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig, Plugin } from 'vite'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [react(),  tailwindcss(), fontPreloadPlugin()],
 
 
   // 포트지정
@@ -23,3 +23,40 @@ export default defineConfig({
     },
   },
 })
+
+
+function fontPreloadPlugin(): Plugin {
+  return {
+    name: 'vite-font-preload',
+    transformIndexHtml: {
+      order: 'pre', // 기존 enforce: 'pre'
+      handler(html) {
+        const fontDir = path.resolve(__dirname, 'src/assets/fonts')
+        const preloadLinks: string[] = []
+
+        function walk(dir: string) {
+          const files = fs.readdirSync(dir)
+          for (const file of files) {
+            const fullPath = path.join(dir, file)
+            const stat = fs.statSync(fullPath)
+
+            if (stat.isDirectory()) {
+              walk(fullPath)
+            } else if (file.endsWith('.woff') || file.endsWith('.woff2')) {
+              const publicPath = fullPath.split('assets')[1].replace(/\\/g, '/')
+              const type = file.endsWith('.woff2') ? 'font/woff2' : 'font/woff'
+
+              preloadLinks.push(
+                  `<link rel="preload" href="/assets${publicPath}" as="font" type="${type}" crossorigin>`
+              )
+            }
+          }
+        }
+
+        walk(fontDir)
+
+        return html.replace('</head>', preloadLinks.join('\n') + '\n</head>')
+      },
+    },
+  }
+}
